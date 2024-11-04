3a2272455252f8ecfc7b7f31a3d9da40cd874057
Guilherme Orozimbo <guilhermegv890@gmail.com>
2024-11-04 18:58:21 -0300

    Commit initial project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+ <div id="nav">
+  <router-link>
+    <img src="" alt="" id="logo">
+  </router-link>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+ </div> 
+</template>
+
+<script>
+export default{
+  name: "Navbar",
+  data() {
+    return {
+      selected: null,
+      options: [
+        { value: 1, text: 'Option 1' },
+        { value: 2, text: 'Option 2' },
+        { value: 3, text: 'Option 3' },
+        { value: 4, text: 'Option 4' },
+        { value: 5, text: 'Option 5' },
+      ],
+    }
+  },
+  methods: {
+    handleSelect(selected) {
+      this.selected = selected;
+    },
+  },
+}
+</script>
+<style scoped>
+/* CSS styles */
+.custom-select {
+  width: 200px;
+  border: 1px solid #ccc;
+  padding: 5px;
+  border-radius: 5px;
+  background-color: #fff;
+}
+
+</style>
